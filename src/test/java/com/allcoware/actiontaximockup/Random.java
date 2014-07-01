2d8346adc30ccffcd62e4676c19cd27a15e15f4d
alfred <alfred@alfred-pc>
2014-07-01 18:10:05 -0400

    testing this out

---
+++ src/test/java/com/allcoware/actiontaximockup/Random.java
@@ -0,0 +1,12 @@
+// Error reading included file Templates/Classes/Templates/Licenses/license-gplallcoware.txt
+package com.allcoware.actiontaximockup;
+
+/**
+ *
+ * @author alfred
+ */
+
+
+public class Random {
+    //this is a test man
+}
