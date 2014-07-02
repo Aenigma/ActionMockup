b9e9e4aa6336dcb21fbcd197648ff2057a938631
alfred <alfred@alfred-pc>
2014-07-02 19:21:59 -0400

    update

---
+++ src/main/java/com/allcoware/actiontaximockup/Driver.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 Jon Butler, Sam Morekas,
+ *     Rushikesh Parekh, and Kevin Raoofi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.allcoware.actiontaximockup;
+
+import com.allcoware.actiontaximockup.ui.Cab;
+import java.util.Collection;
+
+/**
+ *
+ * @author alfred
+ */
+
+
+public class Driver {
+    
+    private static long number;
+    Cab cab;
+    CustomMoney money;
+    private static String firstName;
+    private static String middleName;
+    private static String lastName;
+    private static String phone;
+    private static Collection<Transaction> transaction;
+    private static Collection<RecurringTransaction> recurringTransactions;
+    
+    
+    
+}
